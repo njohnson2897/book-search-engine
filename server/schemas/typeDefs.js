ffda39a5afa02120234be6a4ed4c924d43a10855
Nate Johnson <njohnson2897@gmail.com>
2024-05-03 08:04:34 -0500

    adds schemas and builds out typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const typeDefs = `
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(_____, description: String!, title: String!, bookId: String!, image: _____, link: ____): User
+        removebook(bookId: String!): User
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int!
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String!]
+        description: String!
+        title: String!
+        image:
+        link:
+    }
+
+    type Auth {
+        token: 
+        user: User
+    }
+    `
